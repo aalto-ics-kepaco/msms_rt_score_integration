1c0a45bb1a267200ac86ec2ee4c1239f2523c3d4
--help <eric.bach@aalto.fi>
2020-07-23 10:10:50 +0300

    update description in setup-file

--- setup.py
+++ setup.py
@@ -16,12 +16,14 @@ setup(
         "matplotlib>=3.1",
         "seaborn>=0.9.0",
         "networkx>=2.4",
-        "setuptools >= 46.1"
+        "setuptools>=46.1"
     ],
 
     # Metadata
     author="Eric Bach",
     author_email="eric.bach@aalto.fi",
-    description="Implementation of our MS2 + RT score integration framework.",
+    description="Implementation of an MS + RT score integration framework. The details of the framework are described"
+                "in 'Probabilistic Framework for Integration of Mass Spectrum and Retention Time Information in "
+                "Metabolite Identification' by Eric Bach, Simon Rogers, John Williamson and Juho Rousu (2020).",
     url="https://github.com/aalto-ics-kepaco/msms_rt_score_integration",
 )
